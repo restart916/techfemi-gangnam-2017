9362dae1f27cf8c9461579feeba002ebe148bcf3
yongsang <yongsang@publy.co>
2017-05-13 23:00:49 +0900

    fixed bug

--- functions/index.js
+++ functions/index.js
@@ -28,9 +28,9 @@ exports.test3 = functions.database.ref('/comments/{objectId}/comment')
 			return event.data.ref.parent.remove();
 	    }
 
-	    let datetime = new Date(),
+	    let datetime = new Date();
 	    return event.data.ref.parent.child('datetime').set(datetime);
-	    
+
 	    // You must return a Promise when performing asynchronous tasks inside a Functions such as
 	    // writing to the Firebase Realtime Database.
 	    // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
